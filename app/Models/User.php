85bbb77123a75dcd28f8c12e4ddb07778de629c0
Risfandi Bayu <risfandibayu19@gmail.com>
2022-03-19 15:04:02 +0700

    r

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function trader()
     {
-        return $this->hasOne(trader::class,'user_id')->withDefault();
+        return $this->hasOne(trader::class,'user_id');
     }
 
     protected $connection = 'mysql';
