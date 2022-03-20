700049d3f227d4d3705db0dac7f3fb911574036f
Risfandi Bayu <risfandibayu19@gmail.com>
2022-03-20 21:32:24 +0700

    register login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -31,7 +31,7 @@ class LoginController extends Controller
     protected function redirectTo()
     {
         if (Auth::user()->role_id == 2) {
-            return '/user';
+            return '/';
         } else if (Auth::user()->role_id == 1) {
             return '/admin';
         } else {
