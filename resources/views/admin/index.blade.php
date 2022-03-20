f7f1c0b77faee96ac56c62bad730e36aaee516b7
Risfandi Bayu <risfandibayu19@gmail.com>
2022-03-20 22:27:58 +0700

    s

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,6 +11,7 @@
                     <div class="col-12 mb-1">
                         <h4>Welcome {{Auth::user()->trader->name}}!</h4>
                         <p>Platform Equity Crowdfunding pertama yang berizin dan diawasi Otoritas Jasa Keuangan berdasarkan Surat Keputusan Nomor: KEP-59/D.04/2019.</p>
+                        
                     </div>
                 </div>
 
