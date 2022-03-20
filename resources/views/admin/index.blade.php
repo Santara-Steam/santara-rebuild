da8622f520ea0fc316dcf95a5a8a429a85352113
Risfandi Bayu <risfandibayu19@gmail.com>
2022-03-20 22:11:14 +0700

    c

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,11 +9,8 @@
             <section id="basic-examples">
                 <div class="row">
                     <div class="col-12 mb-1">
-                        <h4 class="text-uppercase">Welcome Admin Santara.co.id</h4>
-                        <p>Cards require a small amount of markup and classes to provide you with as much control as possible. These classes
-                            and markup are flexible though and can typically be remixed and extended with ease. For example, if your card has
-                            no flush content like images, feel free to put the <code>.card-body</code> class on the <code>.card</code> element
-                            to consolidate your markup.</p>
+                        <h4>Welcome {{Auth::user()->trader->name}}!</h4>
+                        <p>Platform Equity Crowdfunding pertama yang berizin dan diawasi Otoritas Jasa Keuangan berdasarkan Surat Keputusan Nomor: KEP-59/D.04/2019.</p>
                     </div>
                 </div>
 
