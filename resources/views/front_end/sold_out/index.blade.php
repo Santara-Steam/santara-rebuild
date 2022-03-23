7472a2e80b7018986d0165df66e3bfdef2a17b38
bagas dwi <bagasdwi245@gmail.com>
2022-03-23 12:02:30 +0700

    index sold

--- resources/views/front_end/sold_out/index.blade.php
+++ resources/views/front_end/sold_out/index.blade.php
@@ -65,23 +65,10 @@
                           @foreach ($sold_out as $item)
                           <?php 
                           $picture = explode(',',$item->pictures);
-                          $tot=number_format(round($item->supply * $item->price),0,',','.');
-                          
                           ?>
-                    <div class="col-lg-3 col-sm-3 col-3 moldla" style="padding: 5px;">
-                      <a type="button" data-toggle="modal" id="detail_sold" style="width: 100%;"
-                            class="mod_sold detail_sold moldla" data-target="#modal_sold" data-ktg_sold="<?=$item->ktg?>"
-                            data-trademark_sold="<?=$item->trademark?>" data-company_name_sold="<?=$item->company_name?>"
-                            data-tot_pendanaan_sold="<?=$tot?>" data-image_sold="<?=$picture[0]?>">
-                          <div class="card moldla">
-                            <img class="rectangle-2 moldla"
-                              src="https://storage.googleapis.com/asset-santara/santara.co.id/token/{{$picture[0]}}" />
-                          </div>
-                      </a>
-                    </div>
-                          <a type="button" class="molpli" data-toggle="modal" data-target="#exampleModal">
-                           <div class="col-lg-3 col-sm-3 col-3 molpli" style="padding: 5px;">
-                              <div class="card molpli" style="margin-top: 10px;">
+                          
+                           <div class="col-lg-3 col-sm-3 col-3" style="padding: 5px;">
+                              <div class="card" style="margin-top: 10px;">
                                 <img class="rectangle-2"  src="https://storage.googleapis.com/asset-santara/santara.co.id/token/{{$picture[0]}}" />
                                 <div class="content">
                                   <div class="header-card-dan-progress-2">
@@ -127,7 +114,7 @@
                                 </div>
                               </div>
                            </div>
-                          </a>
+                           
                           @endforeach
                         </div>
                         <div>
@@ -144,101 +131,4 @@
             </div>
          </div>
       </div>
-
-      <div class="modal fade" id="modal_sold" tabindex="-1" role="dialog" aria-labelledby="detail_sold"
-      aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="card" style="margin-bottom: -1px;">
-            <img class="rectangle-2" id="image_sold" />
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"
-              style="margin-right: 10px; margin-top: 0px; width: 30px;">
-              <span aria-hidden="true">&times;</span>
-            </button>
-            <div class="content2">
-              <div class="header-card-dan-progress-2">
-                <div class="header-and-tags">
-                              <span class="tx-t inter-medium-sweet-pink-12px" style="background: var(--falu-red);
-    border-radius: 10px; box-shadow: 10px 0 0 var(--falu-red), 0px 0 0 var(--falu-red); line-height : 20px; padding-left:10px;" id="ktg_sold"></span>
-                                <div class="header">
-                    <div class="saka-logistics inter-medium-alabaster-20px">
-                      <span class="tx-pt inter-medium-alabaster" id="trademark_sold">
-                      </span>
-                    </div>
-                    <div class="pt-saka-multitrans-nusantara inter-normal-quill-gray-12px">
-                      <span class="tx-np inter-normal-quill-gray" id="company_name_sold">
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div class="info-pendanaan">
-                  <div class="mul inter-normal-mercury-14px">
-                    <span class="tx-sold span-1 inter-normal-quill-gray">Total Pendanaan</span>
-                  </div>
-                </div>
-                <div class="addr inter-bold-white-14px">
-                  <span class="tx-sold inter-bold-white" style="font-weight: bold" id="tot_pendanaan_sold">Rp</span>
-                </div>
-                <div>
-                </div>
-              </div>
-              <div class="footer-card">
-                <img class="divider" src="{{ asset('public/assets/images/divider-108@2x.png') }}" />
-                <div class="footer-card-2">
-                  <div class="deviden-dibagikan-rp inter-normal-mercury-12px">
-                    <span class="inter-normal-quill-gray-12px">Deviden Dibagikan<br /></span><span
-                      class="inter-medium-alabaster-12px">Rp250.000.000</span>
-                  </div>
-                  <div class="pembagian-dividen-1-kali inter-normal-mercury-10px">
-                    <span class="inter-normal-quill-gray-12px">Pembagian Dividen<br /></span><span
-                      class="inter-medium-alabaster-12px">1 Kali</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="modal-footer" style="background-color: var(--shark);">
-            <a class="b-daf btn btn-danger btn-lg btn-block" data-toggle="modal" data-target="#exampleModal">Selengkapnya</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Maaf</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      Halaman masih dalam pengembangan.
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<script>
-  $(document).ready(function() {
-    $(document).on('click', '.detail_sold', function() {
-      var ktg_sold = $(this).data('ktg_sold');
-      var trademark_sold = $(this).data('trademark_sold');
-      var company_name_sold = $(this).data('company_name_sold');
-      var image_sold = $(this).data('image_sold');
-      var tot_pendanaan_sold = $(this).data('tot_pendanaan_sold');
-      var periode_dividen_sold = $(this).data('periode_dividen_sold');
-      $('#ktg_sold').text(ktg_sold);
-      $('#trademark_sold').text(trademark_sold);
-      $('#company_name_sold').text(company_name_sold);
-      $('#image_sold').prop('src', 'https://storage.googleapis.com/asset-santara/santara.co.id/token/' + image_sold);
-      $('#tot_pendanaan_sold').text(tot_pendanaan_sold);
-      $('#periode_dividen_sold').text(periode_dividen_sold);
-    })
-  })
-</script>
 @endsection
