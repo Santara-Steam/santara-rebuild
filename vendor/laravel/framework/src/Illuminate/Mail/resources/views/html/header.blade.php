049a972f93bd9e56c6fc262690076392948abc17
Risfandi Bayu <risfandibayu19@gmail.com>
2022-03-24 11:05:40 +0700

    email

--- vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/header.blade.php
+++ vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+<img src="http://dev.santara.co.id/public/admin/img/santara-tidur-dark.png" style="width: 200px;" alt=" ">
 @else
 {{ $slot }}
 @endif
