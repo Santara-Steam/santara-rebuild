0adcbb85822c003318011e597f3c27aeac2efcc2
bagas dwi <bagasdwi245@gmail.com>
2022-03-25 08:44:43 +0700

    share detail

--- resources/views/front_end/coming_soon/show.blade.php
+++ resources/views/front_end/coming_soon/show.blade.php
@@ -490,55 +490,56 @@
             </div>
           </div>
           <div class="row mt-3 mb-3 d-flex justify-content-center ">
-            <!-- <div class="col-4 col-md-2">
-                    <img width="50px" src="https://santara.co.id/assets/new-santara/img/sosmed/instagram.png" />
-                    <p class="ff-n fs-12 mt-2" style="color: #708088;">Instagram</p>
-                </div> -->
-            <div class="col-4 col-md-2">
-              <a href="https://www.facebook.com/sharer/sharer.php?u=https://dev.santara.co.id/detail-coming-soon/{{$emt->id}}"
-                id="shareFacebook" target="_blank" style="text-decoration: none;">
-                <img width="50px" src="https://santara.co.id/assets/new-santara/img/sosmed/facebook.png"
-                  class="lazyload">
-                <p class="ff-n fs-12 mt-2" style="color: #708088;">Facebook</p>
-              </a>
+              <!-- <div class="col-4 col-md-2">
+                      <img width="50px" src="https://santara.co.id/assets/new-santara/img/sosmed/instagram.png" />
+                      <p class="ff-n fs-12 mt-2" style="color: #708088;">Instagram</p>
+                  </div> -->
+              <div class="col-4 col-md-2">
+                <a href="https://www.facebook.com/sharer/sharer.php?u={{url('detail-coming-soon')}}/{{$emt->id}}"
+                  id="shareFacebook" target="_blank" style="text-decoration: none;">
+                  <img width="50px" src="https://santara.co.id/assets/new-santara/img/sosmed/facebook.png"
+                    class="lazyload">
+                  <p class="ff-n fs-12 mt-2" style="color: #708088;">Facebook</p>
+                </a>
+              </div>
+              <div class="col-4 col-md-2">
+                <a href="https://twitter.com/intent/tweet?url={{url('detail-coming-soon')}}/{{$emt->id}}&amp;text=Temukan%20peluang%20investasi%20berikut%20di%20Santara!%0A PT. Lembu Sora Lampung"
+                  id="shareTwitter" style="text-decoration: none;" target="_blank">
+                  <img width="50px" src="https://santara.co.id/assets/new-santara/img/sosmed/twitter.png"
+                    class="lazyload">
+                  <p class="ff-n fs-12 mt-2" style="color: #708088;">Twitter</p>
+                </a>
+              </div>
+              <div class="col-4 col-md-2">
+                <a href="https://telegram.me/share/url?url={{url('detail-coming-soon')}}/{{$emt->id}}&amp;text=Temukan%20peluang%20investasi%20berikut%20di%20Santara!%0A PT. Lembu Sora Lampung"
+                  id="shareTelegram" target="_blank" style="text-decoration: none;">
+                  <img width="50px" src="https://santara.co.id/assets/new-santara/img/sosmed/telegram.png"
+                    class="lazyload">
+                  <p class="ff-n fs-12 mt-2" style="color: #708088;">Telegram</p>
+                </a>
+              </div>
+              <!-- <div class="col-4 col-md-2">
+                      <img width="50px" src="https://santara.co.id/assets/new-santara/img/sosmed/tiktok.png" />
+                      <p class="ff-n fs-12 mt-2" style="color: #708088;">TikTok</p>
+                  </div> -->
+              <div class="col-4 col-md-2">
+                <a href="https://web.whatsapp.com/send?text=Temukan%20peluang%20investasi%20berikut%20di%20Santara!%0A {{url('detail-coming-soon')}}/{{$emt->id}}"
+                  id="shareWhatsapp" target="_blank" style="text-decoration: none;">
+                  <img width="50px" src="https://santara.co.id/assets/new-santara/img/sosmed/whatsapp.png"
+                    class="lazyload">
+                  <p class="ff-n fs-12 mt-2" style="color: #708088;">WhatsApp</p>
+                </a>
+              </div>
             </div>
-            <div class="col-4 col-md-2">
-              <a href="https://twitter.com/intent/tweet?url=https://dev.santara.co.id/detail-coming-soon/{{$emt->id}}&amp;text=Temukan%20peluang%20investasi%20berikut%20di%20Santara!%0A PT. Lembu Sora Lampung"
-                id="shareTwitter" style="text-decoration: none;" target="_blank">
-                <img width="50px" src="https://santara.co.id/assets/new-santara/img/sosmed/twitter.png"
-                  class="lazyload">
-                <p class="ff-n fs-12 mt-2" style="color: #708088;">Twitter</p>
-              </a>
+            <div class="input-group input-group-lg mb-3">
+              <input type="text" id="inputShareLink" class="form-control fs-16 bold ff-n" disabled=""
+                style="border-radius: 25px;padding-right:150px" aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+                placeholder="{{url('detail-coming-soon')}}/{{$emt->id}}">
+              <span id="copy-link" class="input-group-text"
+                style="position: inherit;height: 33px;justify-content: center;align-items: center;margin: 10px 17px 10px -134px;border-radius: 20px;color: #BF2D30;border-color: #BF2D30; cursor:pointer"
+                onclick="shareButton('{{url('detail-coming-soon')}}/{{$emt->id}}')">Copy Link</span>
             </div>
-            <div class="col-4 col-md-2">
-              <a href="https://telegram.me/share/url?url=https://dev.santara.co.id/detail-coming-soon/{{$emt->id}}&amp;text=Temukan%20peluang%20investasi%20berikut%20di%20Santara!%0A PT. Lembu Sora Lampung"
-                id="shareTelegram" target="_blank" style="text-decoration: none;">
-                <img width="50px" src="https://santara.co.id/assets/new-santara/img/sosmed/telegram.png"
-                  class="lazyload">
-                <p class="ff-n fs-12 mt-2" style="color: #708088;">Telegram</p>
-              </a>
-            </div>
-            <!-- <div class="col-4 col-md-2">
-                    <img width="50px" src="https://santara.co.id/assets/new-santara/img/sosmed/tiktok.png" />
-                    <p class="ff-n fs-12 mt-2" style="color: #708088;">TikTok</p>
-                </div> -->
-            <div class="col-4 col-md-2">
-              <a href="https://web.whatsapp.com/send?text=Temukan%20peluang%20investasi%20berikut%20di%20Santara!%0A https://dev.santara.co.id/detail-coming-soon/{{$emt->id}}"
-                id="shareWhatsapp" target="_blank" style="text-decoration: none;">
-                <img width="50px" src="https://santara.co.id/assets/new-santara/img/sosmed/whatsapp.png"
-                  class="lazyload">
-                <p class="ff-n fs-12 mt-2" style="color: #708088;">WhatsApp</p>
-              </a>
-            </div>
-          </div>
-          <div class="input-group input-group-lg mb-3">
-            <input type="text" id="inputShareLink" class="form-control fs-16 bold ff-n" disabled=""
-              style="border-radius: 25px;padding-right:150px" aria-label="Recipient's username"
-              aria-describedby="basic-addon2" placeholder="https://dev.santara.co.id/detail-coming-soon/{{$emt->id}}">
-            <span id="copy-link" class="input-group-text"
-              style="position: inherit;height: 33px;justify-content: center;align-items: center;margin: 10px 17px 10px -134px;border-radius: 20px;color: #BF2D30;border-color: #BF2D30; cursor:pointer"
-              onclick="shareButton('https://dev.santara.co.id/detail-coming-soon/{{$emt->id}}')">Copy Link</span>
-          </div>
         </div>
 
       </div>
@@ -709,7 +710,7 @@
 });
 </script>
 <script>
-  function shareButton(url) {
+    function shareButton(url) {
       navigator.clipboard.writeText(url);
       // alert("Copied the text: " + url);
       toastr.success("Url Berhasil Di Copy!!");
@@ -743,31 +744,7 @@
       }
 
   }
-</script>
-<script type='text/javascript'>
-  $(document).ready(function(){
-
-      $('#cmt').click(function(){
-         
-          var id = $(this).data('id');
-
-          // AJAX request
-          $.ajax({
-              url: '{{url("getmodaldata")}}/'+id,
-              type: 'get',
-              data: {id: id},
-              success: function(cmt){ 
-                  // Add response in Modal body
-                  $('.comm').html(cmt); 
-
-                  // Display Modal
-                  // $('#empModal').modal('show'); 
-                  // console.log(cmt);
-              }
-          });
-      });
-});
-</script>
+  </script>
 
 <script type='text/javascript'>
   $(document).ready(function(){
