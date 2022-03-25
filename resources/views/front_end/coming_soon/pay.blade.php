68019da9ddb85818e602748a4efc6de902822d02
Risfandi Bayu <risfandibayu19@gmail.com>
2022-03-25 08:15:35 +0700

    bca

--- resources/views/front_end/coming_soon/pay.blade.php
+++ resources/views/front_end/coming_soon/pay.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <!-- fashion section start -->
-<div class="fashion_section" style="margin-top: 146px;margin-bottom: -200px">
+<div class="fashion_section" style="margin-top: 146px;>
     <div class="container">
         <div class="section-langkah-mudah">
             <div class="langkah-mudah-daftarkan-bisnis-anda inter-normal-alabaster-36px">
@@ -12,42 +12,18 @@
             </div>
             {{-- <div class="content-1"> --}}
                 <div class="row" style="margin-top: 40px">
-                    <div class="col-md-3">
+                    <div class="col-md-12">
                         <div class="card text-white bg-danger mb-3 " style="padding: 20px;">
-                            <div class="card-header" style="background-color: #af2a37;border-radius: 5px;">BCA</div>
+                            <div class="card-header" style="background-color: #ffffff;border-radius: 5px;">
+                                <img src="{{asset('public')}}/bca.png" class="img-fluid" width="100px" alt="">
+                                </div>
                             <div class="card-body " style="padding: 20px 0px 10px 0px;">
-                                <h2 class="c-margin-b-20" style="color: white;font-family: Arial, Helvetica, sans-serif;"> 12313-123123-12313123</h2>
-                                <p class="card-text" style="margin: 0;">A.n. Santara Santara Santara</p>
+                                <h2 class="c-margin-b-20" style="color: white;font-family: Arial, Helvetica, sans-serif;"> 4567255777</h2>
+                                <p class="card-text" style="margin: 0;">A.n. PT Santara Daya Inspiratama</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-3">
-                        <div class="card text-white bg-danger mb-3 " style="padding: 20px;">
-                            <div class="card-header" style="background-color: #af2a37;border-radius: 5px;">BCA</div>
-                            <div class="card-body " style="padding: 20px 0px 10px 0px;">
-                                <h2 class="c-margin-b-20" style="color: white;font-family: Arial, Helvetica, sans-serif;"> 12313-123123-12313123</h2>
-                                <p class="card-text" style="margin: 0;">A.n. Santara Santara Santara</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="card text-white bg-danger mb-3 " style="padding: 20px;">
-                            <div class="card-header" style="background-color: #af2a37;border-radius: 5px;">BCA</div>
-                            <div class="card-body " style="padding: 20px 0px 10px 0px;">
-                                <h2 class="c-margin-b-20" style="color: white;font-family: Arial, Helvetica, sans-serif;"> 12313-123123-12313123</h2>
-                                <p class="card-text" style="margin: 0;">A.n. Santara Santara Santara</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="card text-white bg-danger mb-3 " style="padding: 20px;">
-                            <div class="card-header" style="background-color: #af2a37;border-radius: 5px;">BCA</div>
-                            <div class="card-body " style="padding: 20px 0px 10px 0px;">
-                                <h2 class="c-margin-b-20" style="color: white;font-family: Arial, Helvetica, sans-serif;"> 121234232-4324234234</h2>
-                                <p class="card-text" style="margin: 0;" >A.n. Santara Santara Santara</p>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
                 <div class="row  mb-4">
 
@@ -102,11 +78,11 @@
         <div class="container disclaimer-inner-bg" style="font-size: 11px;">
 
             <h4 class="text-danger ff-a fs-16"
-                style="font-size: 16px; font-family: 'acumin-pro'; margin-left: 5px; margin-bottom: -2px">Disclaimer:
+                style="font-size: 16px; font-family: 'acumin-pro'; margin-left: 20px; margin-bottom: -2px">Disclaimer:
             </h4>
             <div class="row ff-n"
-                style="font-weight: normal;     text-align: justify; margin-right: -15px; font-family: 'Nunito'; font-size: 11px;">
-                <p class="mt-2" style="margin-bottom: -10px; color: #fff; font-size: 11px;line-height:1.5;">Pembelian
+                style="font-weight: normal;     text-align: justify; margin-right: 25px; font-family: 'Nunito'; font-size: 11px;">
+                <p class="mt-2" style="margin-bottom: 8px; color: #fff; font-size: 11px;line-height:1.5;">Pembelian
                     saham bisnis merupakan aktivitas beresiko tinggi. Anda berinvestasi pada bisnis yang mungkin saja
                     mengalami kenaikan dan penurunan kinerja bahkan mengalami kegagalan. Harap menggunakan pertimbangan
                     ekstra dalam membuat keputusan untuk membeli saham. Ada kemungkinan Anda tidak bisa menjual kembali
@@ -115,7 +91,7 @@
                     untuk membeli saham bisnis sebagai investasi. Semua keputusan pembelian merupakan keputusan
                     independen oleh pengguna.
                 </p>
-                <p style="margin-bottom: -10px; color: #fff; font-size: 11px;line-height:1.5; " ;>
+                <p style="margin-bottom: 8px; color: #fff; font-size: 11px;line-height:1.5; " ;>
                     Santara bertindak sebagai penyelenggara urun dana yang mempertemukan pemodal dan penerbit, bukan
                     sebagai pihak yang menjalankan bisnis (Penerbit). Otoritas Jasa Keuangan bertindak sebagai regulator
                     dan pemberi izin, bukan sebagai penjamin investasi. Keputusan pembelian saham, sepenuhnya merupakan
@@ -123,16 +99,16 @@
                     menyetujui seluruh syarat dan ketentuan serta memahami semua risiko investasi termasuk resiko
                     kehilangan sebagian atau seluruh modal.
                 </p>
-                <p style="margin-bottom: -10px;color: #fff; font-size: 11px; line-height:1.5; ">
+                <p style="margin-bottom: 8px;color: #fff; font-size: 11px; line-height:1.5; ">
                     “OTORITAS JASA KEUANGAN TIDAK MEMBERIKAN PERNYATAAN MENYETUJUI ATAU TIDAK MENYETUJUI EFEK INI, TIDAK
                     JUGA MENYATAKAN KEBENARAN ATAU KECUKUPAN INFORMASI DALAM LAYANAN URUN DANA INI. SETIAP PERNYATAAN
                     YANG BERTENTANGAN DENGAN HAL TERSEBUT ADALAH PERBUATAN MELANGGAR HUKUM.”
                 </p>
-                <p style="margin-bottom: -10px;color: #fff; font-size: 11px;  line-height:1.5;">
+                <p style="margin-bottom: 8px;color: #fff; font-size: 11px;  line-height:1.5;">
                     “INFORMASI DALAM LAYANAN URUN DANA INI PENTING DAN PERLU MENDAPAT PERHATIAN SEGERA. APABILA TERDAPAT
                     KERAGUAN PADA TINDAKAN YANG AKAN DIAMBIL, SEBAIKNYA BERKONSULTASI DENGAN PENYELENGGARA.”
                 </p>
-                <p style="margin-bottom: -10px;color: #fff; font-size: 11px; line-height:1.5; ">
+                <p style="margin-bottom: 8px;color: #fff; font-size: 11px; line-height:1.5; ">
                     “PENERBIT DAN PENYELENGGARA, BAIK SENDIRI-SENDIRI MAUPUN BERSAMA-SAMA, BERTANGGUNG JAWAB SEPENUHNYA
                     ATAS KEBENARAN SEMUA INFORMASI YANG TERCANTUM DALAM LAYANAN URUN DANA INI.”
                 </p>
