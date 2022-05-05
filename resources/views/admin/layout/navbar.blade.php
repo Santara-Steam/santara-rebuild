c9ee4d0ecd73110029df1d4a8d8120f5e9469ec1
afif.fatakhul <fatakhulafi11@gmail.com>
2022-05-05 20:52:22 +0700

    Rev Navbar

--- resources/views/admin/layout/navbar.blade.php
+++ resources/views/admin/layout/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav
+<nav style="background-color: #7f1d1d"
     class="header-navbar navbar-expand-lg navbar navbar-with-menu navbar-without-dd-arrow fixed-top navbar-dark navbar-shadow" style="background-color: #7f1d1d">
     <div class="navbar-wrapper">
         <div class="navbar-header">
@@ -8,9 +8,13 @@
                             class="ft-menu font-large-1"></i></a></li>
                 <li class="nav-item mr-auto">
                     <a class="navbar-brand" href="{{url('/')}}">
-                        <img class="brand-logo" alt="modern admin logo" style="width:100%"
-                            src="{{asset('public')}}/assets/images/logo_header.png">
+                        {{-- <img class="brand-logo" alt="modern admin logo" style="width:100%"
+                            src="{{asset('public')}}/assets/images/logo_header.png"> --}}
                         {{-- <h3 class="brand-text">Modern</h3> --}}
+                        <img class="brand-logo" alt="modern admin logo" style="width:auto"
+                            src="{{asset('public')}}/assets/images/logo-newsantara-ai-putih-merah-l-1-27@2x.png">
+                        <h3 class="brand-text" style="font-weight: 800;
+                            font-size: 24px;">santara</h3>
                     </a>
                 </li>
                 <li class="nav-item d-none d-lg-block nav-toggle"><a class="nav-link modern-nav-toggle pr-0"
