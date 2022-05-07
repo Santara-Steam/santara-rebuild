2b7e1c9328b7e1e96aa2ffaaf1fb4e71acbb02f8
Risfandi Bayu <risfandibayu19@gmail.com>
2022-05-07 09:53:39 +0700

    config

--- config/global.php
+++ config/global.php
@@ -1,27 +1,32 @@
 <?php
 
 // Dev
-return [
-    "API_ADMIN_VERSION" => "v3.7.1/",
+if (env('CONFIG_ENV') == 'dev') {
+    # code...
+    return [
+        "API_ADMIN_VERSION" => "v3.7.1/",
     "API_CLIENT_VERSION" => "v3.7.1",
     "BASE_API_CLIENT_URL" => "https://tulabi.com:3801",
     "BASE_API_ADMIN_URL" => "https://tulabi.com:3701/",
     "BASE_FILE_URL" => "https://dev.santara.id",
     "BASE_API_FILE" => "https://tulabi.com:3801",
+    "STORAGE_GOOGLE" => "https://storage.googleapis.com/asset-santara-staging/santara.co.id/",
+    "STORAGE_GOOGLE_BUCKET" => "asset-santara-staging",
+    "STORAGE_BUCKET" => "https://storage.googleapis.com/asset-bucket-staging/"
+];
+}else{
+
+    
+    // Production
+return [
+    "API_ADMIN_VERSION" => "v3.7.1/",
+    "API_CLIENT_VERSION" => "v3.7.1",
+    "BASE_API_CLIENT_URL" => "https://fire.santarax.com:3701",
+    "BASE_API_ADMIN_URL" => "https://fire.santarax.com:3801/",
+    "BASE_FILE_URL" => "https://santara.co.id",
+    "BASE_API_FILE" => "https://fire.santarax.com:3701",
     "STORAGE_GOOGLE" => "https://storage.googleapis.com/asset-santara/santara.co.id/",
     "STORAGE_GOOGLE_BUCKET" => "asset-santara",
-    "STORAGE_BUCKET" => "https://storage.googleapis.com/asset-bucket/"
+    "STORAGE_BUCKET" => "https://storage.googleapis.com/asset-bucket-prod/"
 ];
-
-// Production
-// return [
-//     "API_ADMIN_VERSION" => "v3.7.1/",
-//     "API_CLIENT_VERSION" => "v3.7.1",
-//     "BASE_API_CLIENT_URL" => "https://fire.santarax.com:3701",
-//     "BASE_API_ADMIN_URL" => "https://fire.santarax.com:3801/",
-//     "BASE_FILE_URL" => "https://santara.co.id",
-//     "BASE_API_FILE" => "https://fire.santarax.com:3701",
-//     "STORAGE_GOOGLE" => "https://storage.googleapis.com/asset-santara/santara.co.id/",
-//     "STORAGE_GOOGLE_BUCKET" => "asset-santara",
-//     "STORAGE_BUCKET" => "https://storage.googleapis.com/asset-bucket-prod/"
-// ];
+}
