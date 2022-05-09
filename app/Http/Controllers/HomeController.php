42d5ee9305e7ac56568bc10430e339ff1e60a41f
Risfandi Bayu <risfandibayu19@gmail.com>
2022-05-09 22:40:13 +0700

    rvs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        dd('ok');
+        // return view('home');
     }
     public function indexuser()
     {
