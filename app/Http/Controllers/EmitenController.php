f7d75062030537a46fc9c8ef1cfb92ee213b85c8
afif.fatakhul <fatakhulafi11@gmail.com>
2022-05-10 20:56:23 +0700

    Update Poesing mase Emiten

--- app/Http/Controllers/EmitenController.php
+++ app/Http/Controllers/EmitenController.php
@@ -18,15 +18,14 @@ class EmitenController extends Controller
     //
     public function index(){
         $emiten = emiten::where('emitens.is_deleted',0)
-        ->select('emitens.*','categories.category as ktg', 'eh.status', 'emiten_journeys.title as sts','emiten_journeys.date as sd', 'emiten_journeys.end_date as ed')
-        ->leftjoin('categories', 'categories.id','=','emitens.category_id')
-        ->leftjoin('emiten_status_histories as eh', 'eh.emiten_id', '=', 'emitens.id')
-        ->join('emiten_journeys','emiten_journeys.emiten_id','=','emitens.id')
-        ->where('emitens.is_deleted',0)
-        ->groupBy('emitens.id')
-        ->orderBy('emitens.id', 'DESC')
-        //->whereRaw('emiten_journeys.created_at in (SELECT max(created_at) from emiten_journeys GROUP BY emiten_journeys.emiten_id)')
-        ->get();
+            ->select('emitens.*','categories.category as ktg', 'eh.status', 'emiten_journeys.title as sts','emiten_journeys.date as sd', 'emiten_journeys.end_date as ed')
+            ->leftjoin('categories', 'categories.id','=','emitens.category_id')
+            ->leftjoin('emiten_status_histories as eh', 'eh.emiten_id', '=', 'emitens.id')
+            ->leftJoin('emiten_journeys','emiten_journeys.emiten_id','=','emitens.id')
+            ->leftJoin('transactions as t', 't.emiten_id', '=', 'emitens.id')
+            ->groupBy('emitens.id')
+            //->whereRaw('emiten_journeys.created_at in (SELECT max(created_at) from emiten_journeys GROUP BY emiten_journeys.emiten_id)')
+            ->get();
         
         return view('admin.emiten.index',compact('emiten'));
     }
